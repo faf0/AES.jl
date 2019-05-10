3a5c867b6468cc775b28a76ac9f9d61cceed710c
Fabian Foerg <3429782+faf0@users.noreply.github.com>
2019-05-10 09:06:22 -0700

    Add REQUIRE file
    Resolves package dependencies.

--- src/AES.jl
+++ src/AES.jl
@@ -1,7 +1,6 @@
-
 module AES
-    using LegacyStrings
-    const ASCIIString = LegacyStrings.ASCIIString
+	using LegacyStrings
+	const ASCIIString = LegacyStrings.ASCIIString
 	include("aes-modes.jl")
 	export AESEncrypt, AESDecrypt, AESParameters
 	export AESECB, AESCBC, AESCFB, AESOFB, AESCTR
