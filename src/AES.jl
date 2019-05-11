800b40152a5a0c3c6b058604003d9ab208fe0f79
Fabian Foerg <3429782+faf0@users.noreply.github.com>
2019-05-11 13:49:40 -0700

    Remove LegacyStrings

--- src/AES.jl
+++ src/AES.jl
@@ -1,6 +1,4 @@
 module AES
-	using LegacyStrings
-	const ASCIIString = LegacyStrings.ASCIIString
 	include("aes-modes.jl")
 	export AESEncrypt, AESDecrypt, AESParameters
 	export AESECB, AESCBC, AESCFB, AESOFB, AESCTR
